77066a8f95bc5f049f25efcaa6f1ff20a32e6b58
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-07 13:49:49 -0400

    Create 02-variable.java

---
+++ 01-basics/02-variable.java
@@ -0,0 +1,15 @@
+variables are a way to store information in our computer
+there are lots of different types of data we can define for our variable
+e.g. int for integer
+
+public class intro{
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+}
+
+$ javac intro.java
+$ java intro
