0a872a4938b0fed9b1ecef14e90c201f3b0b6a43
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-07 13:56:37 -0400

    Update 02-variable.java

--- 01-basics/02-variable.java
+++ 01-basics/02-variable.java
@@ -1,3 +1,4 @@
+//Variable
 variables are a way to store information in our computer
 there are lots of different types of data we can define for our variable
 e.g. int for integer
@@ -13,3 +14,20 @@ public class intro{
 
 $ javac intro.java
 $ java intro
+
+
+//Expressions
+public class intro{
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+
+        int myFirstNumber = (10+5)+(2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+    }
+}
+ 
