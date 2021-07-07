56fed9cbf9c82be3ca72c6bc601db3b79110d8e0
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-07 15:35:09 -0400

    Create 08-if-then.java

---
+++ 01-basics/08-if-then.java
@@ -0,0 +1,12 @@
+Execute a certain section of code only if a particular test evaluates to true.
+  this is known as conditional logic.
+Conditional logic checks a condition an dexecute certain code based on whether that condition (expression) is true or false
+  
+public class Main{
+    public static void main (String[] args) {
+      
+        boolean isAlien = false;    //assign operator, here set as false
+        if (isAlien == false)   //equal operator, check/test if it is same
+        System.out.println("It is not an alien!");
+    }
+}
