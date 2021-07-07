9be290d770c8668dc7141595f54dcbed9dbf6f0c
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-07 13:43:32 -0400

    Create 01-hello-world.java

---
+++ 01-basics/01-hello-world.java
@@ -0,0 +1,15 @@
+"public" java keyword is an access modifier that defines the scope.
+"class" java keyword is used to define a class with the name following the keyword.
+"static" java keyword that needs an understanding of other concept, we need static for java to find our method to run the code we add
+"void" java keyword used to indicate that the method will not return anything.
+() - a method declaration are mandatory and can optionally include one or more parameters to pass information to a method 
+{} - code block used to deine a block of code where we will be adding statements to perform certain tasks
+statement - a complete command to be executed and can include one or more expressions
+
+//hello.java
+public class hello{
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
+ 
