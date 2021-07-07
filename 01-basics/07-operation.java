4311a7df7e78bb68aa2c7c9be5fa386061149141
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-07 15:29:29 -0400

    Create 07-operation.java

---
+++ 01-basics/07-operation.java
@@ -0,0 +1,57 @@
+//operator, oeprant, expression
+int myVar = 15+12
+operator '+'
+oeprant '15' '12'
+expression - combining variables, literals, method return values and operators
+
+//comment
+Comments are ignored by the computer, are there for humans
+
+//Code
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult =  " + previousResult);
+        result = result - 1;    // 3 - 1 = 2
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult =  " + previousResult);
+
+        result = result * 10;   // 2 * 10 = 20
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;    // 20 / 5 = 4
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3;    // the remainder of (4 % 3) = 1
+        System.out.println("4 % 3 = " + result);
+
+        // result = result + 1;
+        result++; // 1 + 1 = 2
+        System.out.println("1 + 1 = " + result);
+
+        result--;  // 2 - 1 = 1
+        System.out.println("2 - 1 = " + result);
+
+        // result = result +2;
+        result += 2;  // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        // result = result * 10;
+        result *= 10; // 3 * 10 = 30
+        System.out.println("3 * 10 = " + result);
+
+        // result = result / 3
+        result /= 3;  // 30 / 3 = 10
+        System.out.println("30 / 3 = " + result);
+
+        // result = result - 2
+        result -= 2;  // 10 - 2 = 8
+        System.out.println("10 - 2 = " + result);
+
+
+    }
+}
