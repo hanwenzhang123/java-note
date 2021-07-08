51700e6185ba184b8b7de1d9f2bc751967126937
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-08 18:33:41 -0400

    Update 13-methods.java

--- 01-basics/13-methods.java
+++ 01-basics/13-methods.java
@@ -43,3 +43,31 @@ in programming terms, -1 is conventionally used to indicate an error
 and
 in searching algorithms -1 indicates an invalid value or a value not found
      
+//Assigned variable to the function 
+public class Main {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+        
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);     //the result sent back from the finalScore in the calculateScore() below
+        System.out.println("Your final score was " + highScore);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);     //the result sent back from the finalScore in the calculateScore() below
+        System.out.println("Your final score was " + highScore);
+    }
+    
+    public static int calculateScore(boolean gameOver,int score, int levelCompleted, int bonus) {  
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            return finalScore; 
+        }  
+        return -1; 
+    }
+}
