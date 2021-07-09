00e4bbb21532f0bb0ce0035227fe7947853bb1f6
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-08 20:38:34 -0400

    Create 16-for-statement.java

---
+++ 01-basics/16-for-statement.java
@@ -0,0 +1,7 @@
+for statement is often referred to as the for loop and the idea is that it repeatedly loop something until a condition is satisfied 
+when I say repeatedly loops its processing a certain amount of code a code block 
+in other words a given number of times or until a particular condition is satisfied
+
+for(init; termination; increment) {
+        
+}
