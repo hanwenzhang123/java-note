a1a9e69f3e2e9de7622e24e4e5092bb6bfda595a
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-09 16:56:54 -0400

    Update 18-reading-input.java

--- 01-basics/18-reading-input.java
+++ 01-basics/18-reading-input.java
@@ -8,17 +8,29 @@ public class Main {
         Scanner scanner = new Scanner(System.in);       //first thing, scanner the input and talk with the program
 
         System.out.println("Enter your year of birth:");
-        int yearOfBirth = scanner.nextInt();        //scan it ( for int ) and save the input to the yearOfBirth
-      
-        scanner.nextLine(); // handle next line character (enter key), without it, we are not able to enter our name, it would just print out what after
+        
+        boolean hasNextInt = scanner.hasNextInt();  //here we check if the input is int, if it is int then true, if not then false
+        
+        if(hasNextInt){
+            
+            int yearOfBirth = scanner.nextInt();        //scan it ( for int ) and save the input to the yearOfBirth
 
-        System.out.println("Enter your name:");
-        String name = scanner.nextLine();   //scan it and save the input to the name
+            scanner.nextLine(); // handle next line character (enter key), without it, we are not able to enter our name, it would just print out what after
 
-        int age = 2021 - yearOfBirth;   //current year - year of birth to see how old is the person
+            System.out.println("Enter your name:");
+            String name = scanner.nextLine();   //scan it and save the input to the name
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old."); //retrieve the information from the console
+            int age = 2021 - yearOfBirth;   //current year - year of birth to see how old is the person
 
+            if(age >= 0 && age <= 100) {    //condition
+                    System.out.println("Your name is " + name + ", and you are " + age + " years old."); //retrieve the information from the console if meets condition
+            } else {
+                System.out.println("Invalid year of birth");    //if the condition is not met
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
+        
         scanner.close();    //close the scanner at the end
     }
 }
