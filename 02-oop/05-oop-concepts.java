ca1f6914f98a4e86fb1b403e34835cd19eaddb5b
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-11 15:39:13 -0400

    Create 05-oop-concepts.java

---
+++ 02-oop/05-oop-concepts.java
@@ -0,0 +1,14 @@
+reference vs object vs instance vs class
+  
+"class" - a blueprint for a house, using the blueprint(plans) we can build as many houses as we like based on those plan.
+  each house you build (in other words "instantiate" using the "new" operator) is an "object" also known as an "instance".
+  each house you build has an address (a physical location).
+  in other words if you want to tell someone where you live, you give them your address(perhaps written on a piece of paper)
+    this is known as a "reference".
+  you can copy that "reference" as many times as you like but there is still just one house. 
+    in other words, we are copying the paper that has addess on it not the house itself
+  we can pass "reference" as "parameters" to "constructors" and "methods"
+    
+    
+    
+    
