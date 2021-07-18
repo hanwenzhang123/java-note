dbf92b0ffe6a44a42f231a92789a25aa1656435c
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-17 22:26:21 -0400

    Update 02-packages.java

--- 06-java-codes/02-packages.java
+++ 06-java-codes/02-packages.java
@@ -1,1 +1,12 @@
-
+using "import" to import a package
+  like import.util.scanner
+  
+Reasons to use packages
+- programmers can easily determine thaat the classes are related
+- it is easy to know where to find the classes and interfaces that can provide the functions provided by the package
+- because the package creates a new namespace, class and interface name conflicts are avoided
+- classes within the package can have unrestricted acccess to one another while still restricting access for classes outside of package
+  
+  
+  
+  
