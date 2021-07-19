5635cca72ba4841294e7b9174b1d53deb1dc04d5
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-19 12:19:09 -0400

    Update 01-generics.java

--- 05-generics/01-generics.java
+++ 05-generics/01-generics.java
@@ -1,1 +1,27 @@
-type parameters
+//Generics Introduction
+Generics allow us to create classes interfaces and methods that takes types as parameters called type parameters
+
+//Example
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+      //ArrayList items = new ArrayList();    //it is a array list can be anything, no specific type indicated
+        ArrayList<Integer> items = new ArrayList<>(); //type - <Integer>, set the progject structure as lambda so no needs to specify the second one 
+        items.add(1);
+        items.add(2);
+        items.add(3);
+//        items.add("tim");
+        items.add(4);
+        items.add(5);
+
+        printDoubled(items);
+    }
+
+    private static void printDoubled(ArrayList<Integer> n) {  //indicate the type of the data
+        for(int i : n) {  // (Object i : n)   //for each i in n
+            System.out.println( i * 2); // ((Integer) i * 2)
+        }
+    }
+}
