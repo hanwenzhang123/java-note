f2a23119cc2c691e4908e016e3c88e42fe6e6b85
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-19 15:33:35 -0400

    Update 02-generics-class.java

--- 05-generics/02-generics-class.java
+++ 05-generics/02-generics-class.java
@@ -1,1 +1,158 @@
+//Abstract Class - Player.java
+public abstract class Player {    //rules
+    private String name;
 
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+//BaseballPlayer.java
+public class BaseballPlayer extends Player {
+    public BaseballPlayer(String name) {    //the name stored in the player
+        super(name);
+    }
+}
+
+//FootballPlayer.java
+public class FootballPlayer extends Player {
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
+
+//SoccerPlayer.java
+public class SoccerPlayer extends Player {
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+}
+
+//Team.java
+import java.util.ArrayList;
+
+public class Team<T extends Player> implements Comparable<Team<T>> {    //type parameter, makes it more generics
+    private String name;
+    int played = 0;
+    int won = 0;
+    int lost = 0;
+    int tied = 0;
+
+    private ArrayList<T> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addPlayer(T player) {   //either added sucessfully or not
+        if (members.contains(player)) {
+            System.out.println(player.getName() + " is already on this team");
+            return false;
+        } else {
+            members.add(player);
+            System.out.println(player.getName() + " picked for team " + this.name);
+            return true;
+        }
+    }
+
+    public int numPlayers() {   //retutrn the number of the player
+        return this.members.size();
+    }
+
+    public void matchResult(Team<t> opponent, int ourScore, int theirScore) {
+      
+        String message; 
+      
+        if(ourScore > theirScore) {
+            won++;
+            message = " beat ";
+        } else if(ourScore == theirScore) {
+            tied++;
+            message = " drew with ";
+
+        } else {
+            lost++;
+            message = " lost to ";
+        }
+      
+        played++;
+        if(opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
+            opponent.matchResult(null, theirScore, ourScore); //call it again to pass null and save results
+        }
+    }
+
+    public int ranking() {
+        return (won * 2) + tied;
+    }
+  
+    @Override
+    public int compareTo(Team<T> team) {
+        if(this.ranking() > team.ranking()) {
+            return -1;
+        } else if(this.ranking() < team.ranking()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+}
+
+//Main.java
+public class Main {
+
+    public static void main(String[] args) {
+	    FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");    //here we assign the type the <T>
+        adelaideCrows.addPlayer(joe);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("this won't work");
+        brokenTeam.addPlayer(beckham);
+
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gordon");
+        melbourne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorn= new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle= new Team<>("Fremantle");
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(adelaideCrows, 3, 8);
+
+        adelaideCrows.matchResult(fremantle, 2, 1);
+//        adelaideCrows.matchResult(baseballTeam, 1, 1);  not working
+
+        System.out.println("Rankings");
+        System.out.println(adelaideCrows.getName() + ": " + adelaideCrows.ranking());
+        System.out.println(melbourne.getName() + ": " + melbourne.ranking());
+        System.out.println(fremantle.getName() + ": " + fremantle.ranking());
+        System.out.println(hawthorn.getName() + ": " + hawthorn.ranking());
+
+        System.out.println(adelaideCrows.compareTo(melbourne));
+        System.out.println(adelaideCrows.compareTo(hawthorn));
+        System.out.println(hawthorn.compareTo(adelaideCrows));
+        System.out.println(melbourne.compareTo(fremantle));
+
+        ArrayList<Team> teams;
+        Collections.sort(teams);
+    }
+}
