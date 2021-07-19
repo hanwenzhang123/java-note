538acca5ea16037a1644ef809e4a2c1f582ee092
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-18 20:23:49 -0400

    Update 02-packages.java

--- 06-java-codes/02-packages.java
+++ 06-java-codes/02-packages.java
@@ -1,4 +1,4 @@
-using "import" to import a package
+using "import" to import package, a way to group related classes and interfaces together
   like import.util.scanner
   
 Reasons to use packages
@@ -8,5 +8,41 @@ Reasons to use packages
 - classes within the package can have unrestricted acccess to one another while still restricting access for classes outside of package
   
   
+//MyWindow.java
+import java.awt.*;  //all the classes and interfaces * like the Frame
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
   
-  
+public class MyWindow extends Frame {
+
+    public MyWindow(String title) {
+        super(title);
+        setSize(500, 140);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Font sansSerifLarge = new Font("SansSerif", Font.BOLD, 18);
+        Font sansSerifSmall = new Font("SansSerif", Font.BOLD, 12);
+        g.setFont(sansSerifLarge);
+        g.drawString("The Complete Java Developer Course", 60, 60);
+        g.setFont(sansSerifSmall);
+        g.drawString("by Tim Buchalka", 60, 100);
+    }
+}
+
+//Main.java
+public class Main {
+
+    public static void main(String[] args) {
+	    MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+    }
+}
