d39af358fb6a6c1a7251909e1d5eedcb668b25fe
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-19 23:30:44 -0400

    Update 00-online-resources.java

--- 03-arrays/00-online-resources.java
+++ 03-arrays/00-online-resources.java
@@ -18,3 +18,21 @@ You can't resize an array in Java. You'd need to either:
 2. Use the java.util.ArrayList<T> class, which does this for you when you need to make the array bigger. It nicely encapsulates what you describe in your question.
 3. Use java.util.Arrays.copyOf(...) methods which returns a bigger array, with the contents of the original array.
 - https://stackoverflow.com/questions/13197702/resize-an-array-while-keeping-current-elements-in-java
+
+
+//ArrayList
+import java.util.ArrayList;
+
+public class ArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("book");
+        list.add("car");
+        System.out.prinln(list.get(0));
+      
+        list.remove("book");  //remove the item
+        list.clear(); //remove everything
+        list.contain("fruit");  //boolean
+    }
+}
+
