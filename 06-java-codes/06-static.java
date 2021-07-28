688ea1cd39647332ca8d2d9de3be2659227321b9
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-28 13:11:08 -0400

    Update 06-static.java

--- 06-java-codes/06-static.java
+++ 06-java-codes/06-static.java
@@ -1,4 +1,9 @@
 Static - share one copy of that variable with all instances of the class
+    
+- Static methods and variables are associated with the class as a whole, not objects of the class.
+- Static methods and variables are declared as static by using the static keyword upon declaration.
+- Static methods cannot interact with non-static instance variables. This is due to static methods not having a this reference.
+- Both static methods and non-static methods can interact with static variables.
 
 //StaticTest.java
 public class StaticTest {
